9e495d93b4ab0eeaf8b8f0daa39da6fab39922a2
Gregory Tordeur <gregorytordeur@MacBook-Pro-van-Havo.local>
2020-11-06 15:20:53 +0100

    add labeled statement

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -175,6 +175,18 @@ function printLogHandler() {
   //   j++;
   // }
 
+  j = 0;
+  outerWhile: do {
+    console.log('Outer', j);
+    innerFor: for (let k = 0; k < 5; k++) {
+      if (k === 3) {
+        break outerWhile
+      }
+      console.log('Inner', k)
+    }
+    j++;
+  } while (j<3);
+
   let i = 0;
   for (const logEntry of battleLog) {
     if ((!lastLoggedEntry && lastLoggedEntry !== 0) || lastLoggedEntry < i ) {
